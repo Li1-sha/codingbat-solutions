b375c350852fdff92eaf220523753fc2f0ae1957
Shahad Al Harthy <160311849+Li1-sha@users.noreply.github.com>
2025-07-24 14:14:19 +0400

    Delete monkeyTrouble.java out of folder

--- monkeyTrouble.java
+++
@@ -1,8 +0,0 @@
-public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
-  if(aSmile && bSmile)
-    return true;
-  else if(!aSmile && !bSmile)
-    return true;
-  else
-    return false;
-}
