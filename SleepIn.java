9d8722628694389654f3c4189611f89c01188c1e
Shahad Al Harthy <160311849+Li1-sha@users.noreply.github.com>
2025-07-24 14:11:59 +0400

    Delete SleepIn.java out of folder

--- SleepIn.java
+++
@@ -1,8 +0,0 @@
-public boolean sleepIn(boolean weekday, boolean vacation) {
-  if(!weekday && !vacation)
-    return true;
-  else if(weekday && !vacation)
-    return false;
-  else
-    return true;
-};
