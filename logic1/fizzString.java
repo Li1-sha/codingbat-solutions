2bf48400593f1fd051c9f4fff9d2c84620b1db70
Shahad Al Harthy <160311849+Li1-sha@users.noreply.github.com>
2025-07-28 09:48:22 +0400

    Add solution for logic1-fizzString

---
+++ logic1/fizzString.java
@@ -0,0 +1,13 @@
+public String fizzString(String str) {
+  boolean startsWithF = str.startsWith("f");
+  boolean endsWithB = str.endsWith("b");
+
+  if (startsWithF && endsWithB) 
+      return "FizzBuzz";
+  else if (startsWithF)
+      return "Fizz";
+  else if (endsWithB) 
+      return "Buzz";
+  else 
+      return str;
+}
