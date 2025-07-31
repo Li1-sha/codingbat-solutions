972c754c5fdc7620f7d87afe2808f3a79d5737f9
Shahad Al Harthy <160311849+Li1-sha@users.noreply.github.com>
2025-07-31 16:13:03 +0400

    Add solution for map2-wordMultiple

---
+++ map2/wordMultiple.java
@@ -0,0 +1,10 @@
+public Map<String, Boolean> wordMultiple(String[] strings) {
+  Map<String, Boolean> map = new HashMap<>();
+  for (String s : strings) {
+    if (!map.containsKey(s)) 
+      map.put(s, false);
+    else
+      map.put(s, true);
+  }
+  return map;
+}
