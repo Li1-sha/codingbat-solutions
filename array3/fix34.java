04c1cb03ea47b303fd023b8b6219251f61c72875
Shahad Al Harthy <160311849+Li1-sha@users.noreply.github.com>
2025-07-31 17:58:57 +0400

    Add solution for array3-fix34

---
+++ array3/fix34.java
@@ -0,0 +1,17 @@
+public int[] fix34(int[] nums) {
+  int j = 0;
+  for (int i = 0; i < nums.length - 1; i++) {
+    if (nums[i] == 3 && nums[i + 1] != 4) {
+      while (j < nums.length) {
+        if (nums[j] == 4 && (j == 0 || nums[j - 1] != 3)) {
+          int temp = nums[i + 1];
+          nums[i + 1] = nums[j];
+          nums[j] = temp;
+          break;
+        }
+        j++;
+      }
+    }
+  }
+  return nums;
+}
