a8b0646aef7458a53bf32a6e9b3572b17da63607
Shahad Al Harthy <160311849+Li1-sha@users.noreply.github.com>
2025-07-31 17:22:03 +0400

    Add solution for array2-twoTwo

---
+++ array2/twoTwo.java
@@ -0,0 +1,11 @@
+public boolean twoTwo(int[] nums) {
+  for (int i = 0; i < nums.length; i++) {
+    if (nums[i] == 2) {
+      boolean hasNeighbor = (i > 0 && nums[i - 1] == 2) || (i < nums.length - 1 && nums[i + 1] == 2);
+      if (!hasNeighbor) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
