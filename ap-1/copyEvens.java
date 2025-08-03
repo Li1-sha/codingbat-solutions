3a8d1ca57845f0bbd86d2af8b50148ae1d78275a
Shahad Al Harthy <160311849+Li1-sha@users.noreply.github.com>
2025-08-03 15:30:15 +0400

    Add solution for ap1-copyEvens

---
+++ ap-1/copyEvens.java
@@ -0,0 +1,12 @@
+public int[] copyEvens(int[] nums, int count) {
+  int[] result = new int[count];
+  int index = 0;
+  for (int num : nums) {
+    if (num % 2 == 0) {
+      result[index] = num;
+      index++;
+      if (index == count) break;
+    }
+  }
+  return result;
+}
