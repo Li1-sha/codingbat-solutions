45f66c23ca9d2375e2ff408cf9574cc94d07e088
Shahad Al Harthy <160311849+Li1-sha@users.noreply.github.com>
2025-08-04 15:23:39 +0400

    Add solution for recursion1-strCopies

---
+++ recursion1/strCopies.java
@@ -0,0 +1,10 @@
+public boolean strCopies(String str, String sub, int n) {
+  if (n <= 0) return true;
+  if (str.length() < sub.length()) return false;
+
+  if (str.startsWith(sub)) {
+    return strCopies(str.substring(1), sub, n - 1);
+  }else{
+    return strCopies(str.substring(1), sub, n);
+  }
+}
